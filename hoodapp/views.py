9f44e548fa94092516b68c3e09bf70789eef7063
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-19 22:59:18 +0300

    ft:this will include addpost functionality

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -81,7 +81,7 @@ def addHealth(request):
     return render(request, 'addHealth.html', {'title':title,'form':form})
 
 @login_required(login_url='login')
-def addPost(request):
+# def addPost(request):
     title='Post your concerns for your neigbourhood to see'
     current_user = request.user
     if request.method == 'POST':
@@ -99,7 +99,7 @@ def addPost(request):
     return render(request, 'addPost.html', {'title':title,'form':form})
 
 @login_required(login_url='login')
-# def addProfile(request):
+def addProfile(request):
     title='Make your own profile'
     current_user = request.user
     if request.method == 'POST':
