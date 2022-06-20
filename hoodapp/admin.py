6755a3b46d358ab5d170ba10dcd951de3a2db681
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-20 18:20:10 +0300

    ft:this will link the models to admin

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -1,6 +1,15 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class ViciniaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hoodapp'
+# class ViciniaConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'hoodapp'
+from django.contrib import admin
+from .models import Profile,Post,Health ,Police,NeighbourHood,Business
+# Register your models here.
+admin.site.register(NeighbourHood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Police)
+admin.site.register(Health)
