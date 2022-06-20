76556ef65b20d2f88312a32c1d62d396bd020caf
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-20 18:21:59 +0300

    bug:this will fix a bug in the urls

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
     path('',include('hoodapp.urls')),
     
