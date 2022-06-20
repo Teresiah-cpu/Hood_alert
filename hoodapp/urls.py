bf7d7880096bf928b1fc468ccf578e9dd64321ed
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-20 21:16:29 +0300

    ft:this will change the url to repath

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -2,20 +2,20 @@
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path as url
+from django.urls import path as url, re_path
 from django.urls import path, include
 
 
 urlpatterns=[
     path('',views.home,name = 'home'),
-    path('details/(\d+)', views.details, name='details'),
+    re_path('details/(\d+)', views.details, name='details'),
     path('add/business/', views.addBusiness, name='business'),
     path('create/post/', views.addPost , name='post'),
     path('myprofile/', views.addProfile , name='profile'),
     path('add/health/', views.addHealth, name='health'),
     path('add/police_post/', views.addPolice, name='police'),
-    path('business/(\d+)', views.business_details, name='business_details'),
-    path('search/(\d+)', views.search_business, name='search_business')
+    re_path('business/(\d+)', views.business_details, name='business_details'),
+    re_path('search/(\d+)', views.search_business, name='search_business')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
